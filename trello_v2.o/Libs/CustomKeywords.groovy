7ae5e8389c1de1f8e242ca343011209f73b59010
unknown <you@example.com>
2021-12-27 02:36:55 -0800

    git commit "newcode"

--- trello_v2.o/Libs/CustomKeywords.groovy
+++ trello_v2.o/Libs/CustomKeywords.groovy
@@ -7,6 +7,12 @@ import java.lang.String
 
 import com.kms.katalon.core.testobject.TestObject
 
+import java.util.ArrayList
+
+import java.util.HashMap
+
+import java.util.List
+
 
 
 def static "boardHomePage.verifyBoardHomePage"(
@@ -247,4 +253,97 @@ def static "HomePage.verifyBoardDelete"(
 
 def static "HomePage.logout"() {
     (new HomePage()).logout()
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.createFile"(
+    	String fullPathWithoutExtension	
+     , 	String template	
+     , 	String extension	) {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).createFile(
+        	fullPathWithoutExtension
+         , 	template
+         , 	extension)
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.deleteTestSuite"(
+    	String testSuiteName	) {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).deleteTestSuite(
+        	testSuiteName)
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.isStringBelongToList"(
+    	String text	
+     , 	java.util.ArrayList<String> list	) {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).isStringBelongToList(
+        	text
+         , 	list)
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.getListExecutedRows"(
+    	String testCaseId	
+     , 	String iterationType	
+     , 	String iterationValue	
+     , 	int dataTotalRows	) {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).getListExecutedRows(
+        	testCaseId
+         , 	iterationType
+         , 	iterationValue
+         , 	dataTotalRows)
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.createNameForNewSuite"(
+    	String oldTestSuitePath	) {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).createNameForNewSuite(
+        	oldTestSuitePath)
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.replaceTSTemplateWithListNewTC"(
+    	String oldTSTemplate	
+     , 	String newTCList	) {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).replaceTSTemplateWithListNewTC(
+        	oldTSTemplate
+         , 	newTCList)
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.getProjectFileName"() {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).getProjectFileName()
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.getExecutedBrowser"() {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).getExecutedBrowser()
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.replaceFailedRowsForBindingData"(
+    	java.util.HashMap<String, java.util.List<java.lang.Integer>> listFailedRowsBelongToTC	
+     , 	String testCaseTemp	) {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).replaceFailedRowsForBindingData(
+        	listFailedRowsBelongToTC
+         , 	testCaseTemp)
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.createConsoleModeFile"(
+    	String relativeTestSuitePath	) {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).createConsoleModeFile(
+        	relativeTestSuitePath)
+}
+
+
+def static "com.kms.katalon.keyword.testsuite.RerunKeyword.removeTCPassedAndReplaceFailedRows"(
+    	String oldTSTemplate	
+     , 	java.util.List<String> listFailTestCases	
+     , 	java.util.HashMap<String, java.util.List<java.lang.Integer>> listFailedRowsBelongToTC	) {
+    (new com.kms.katalon.keyword.testsuite.RerunKeyword()).removeTCPassedAndReplaceFailedRows(
+        	oldTSTemplate
+         , 	listFailTestCases
+         , 	listFailedRowsBelongToTC)
 }
